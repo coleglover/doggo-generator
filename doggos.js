c82ab559dab00f5f0f8c9b9ccf6e9d000461df98
Cole <cole.r.glover@gmail.com>
2022-01-19 11:27:36 -0800

    refactored code to make it more concise

--- doggos.js
+++ doggos.js
@@ -1,24 +1,20 @@
 //import fetch from "node-fetch"
 
-const doggos = document.querySelector(".doggos");
+const doggos = document.querySelector('.doggos');
 
 //fetch returns a promise - similar to a callback. A promise, like callbacks, allows you to deal with things asynchronously.
 
 function addNewDoggo() {
-  const promise = fetch("https://dog.ceo/api/breeds/image/random");
-
-  promise
-    .then((response) => {
-      const processingPromise = response.json();
-      return processingPromise;
-      //return response.json // returns JSON blob
+  fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => {
+      return response.json(); // returns JSON blob
     })
-    .then((processedResponse) => {
-      const img = document.createElement("img");
+    .then(processedResponse => {
+      const img = document.createElement('img');
       img.src = processedResponse.message;
-      img.alt = "Cute doggos";
+      img.alt = 'Cute doggos';
       doggos.appendChild(img);
     });
 }
 
-document.querySelector(".add-doggo").addEventListener("click", addNewDoggo);
+document.querySelector('.add-doggo').addEventListener('click', addNewDoggo);
