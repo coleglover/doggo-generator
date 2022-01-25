64480da7c302c9c02a584f06592e5e14123fa04b
Cole <cole.r.glover@gmail.com>
2022-01-25 09:35:42 -0800

    added styling and  functionality to remove loading icon on image load

--- doggos.js
+++ doggos.js
@@ -1,10 +1,34 @@
 //import fetch from "node-fetch"
-window.addEventListener('load', hideSpinner);
-const doggos = document.getElementById('doggos');
+//window.addEventListener('load', hideSpinner);
+const doggos = document.querySelector('.doggo-crate');
 
 //fetch returns a promise - similar to a callback. A promise, like callbacks, allows you to deal with things asynchronously.
 
-function addNewDoggo() {
+function addLoader() {
+  const loadingWrapper = document.createElement('div');
+  const loader = document.createElement('div');
+
+  loader.classList.add('loading');
+  loadingWrapper.appendChild(loader);
+
+  loadingWrapper.classList.add('loading-wrapper');
+  doggos.appendChild(loadingWrapper);
+
+  return loadingWrapper;
+}
+
+function dogButtonHandler() {
+  console.log('this should run');
+  const newImageWrapper = addLoader();
+  addNewDoggo(newImageWrapper);
+}
+
+function removeLoader(imageWrapper) {
+  const loader = imageWrapper.querySelector('.loading');
+  imageWrapper.removeChild(loader);
+}
+
+function addNewDoggo(imageWrapper) {
   fetch('https://dog.ceo/api/breeds/image/random')
     .then(response => {
       return response.json(); // returns JSON blob
@@ -13,14 +37,23 @@ function addNewDoggo() {
       const img = document.createElement('img');
       img.src = processedResponse.message;
       img.alt = 'Cute doggo';
-      doggos.appendChild(img);
+      removeLoader(imageWrapper);
+      imageWrapper.appendChild(img);
     });
 }
-function hideSpinner() {
-  let d = getElementById('loading');
-  document.removeChild(d);
-}
 
-document.getElementById('add-doggo').addEventListener('click', addNewDoggo);
+document
+  .querySelector('.add-doggo-button')
+  .addEventListener('click', dogButtonHandler);
 
-window.getElementById('loading-wrapper').addEventListener('load', hideSpinner);
+// click button
+// add loader
+// create img
+// remove loader
+//
+//
+//
+
+// dogButtonHandler
+// -calls add contianer + loader
+// -calls addNewDoggo
